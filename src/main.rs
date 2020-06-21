d72daa77348b5e46c1e412341971b349b5dadfef
Ryan Barry <ryan@nuclearice.com>
2020-06-20 17:05:15 -0700

    render a triangle with a graphics pipeline!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::sync::Arc;
 
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
-use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer, DynamicState};
 use vulkano::descriptor::{descriptor_set::PersistentDescriptorSet, PipelineLayoutAbstract};
 use vulkano::device::{Device, DeviceExtensions, Features};
 use vulkano::format::{ClearValue, Format};
+use vulkano::framebuffer::{Framebuffer, Subpass};
 use vulkano::image::{Dimensions, StorageImage};
 use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
-use vulkano::pipeline::ComputePipeline;
+use vulkano::pipeline::{viewport::Viewport, ComputePipeline, GraphicsPipeline};
 use vulkano::sync::GpuFuture;
 
 use image::{ImageBuffer, Rgba};
@@ -45,6 +46,12 @@ void main() {
 "
     }
 }
+
+#[derive(Default, Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+vulkano::impl_vertex!(Vertex, position);
 
 fn main() {
     let instance =
@@ -194,4 +201,146 @@ fn main() {
     let imgoutcontents = imgoutbuf.read().unwrap();
     let imgout = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &imgoutcontents[..]).unwrap();
     imgout.save("gpu-image.png").unwrap();
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage {
+            vertex_buffer: true,
+            ..BufferUsage::none()
+        },
+        false,
+        vec![vertex1, vertex2, vertex3].into_iter(),
+    )
+    .unwrap();
+
+    mod vs {
+        vulkano_shaders::shader! {
+                    ty: "vertex",
+                    src: "
+#version 450
+
+layout(location = 0) in vec2 position;
+
+void main() {
+  gl_Position = vec4(position, 0.0, 1.0);
+}
+"
+        }
+    }
+
+    mod fs {
+        vulkano_shaders::shader! {
+            ty: "fragment",
+            src: "
+#version 450
+
+layout(location = 0) out vec4 f_color;
+
+void main() {
+  f_color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"
+        }
+    }
+
+    let vs = vs::Shader::load(device.clone()).expect("failed to create vertex shader");
+    let fs = fs::Shader::load(device.clone()).expect("failed to create fragment shader");
+
+    let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(),
+                                                                attachments: {
+                                                                    color: {
+                                                                        load: Clear,
+                                                                        store: Store,
+                                                                        format: Format::R8G8B8A8Unorm,
+                                                                        samples: 1,
+                                                                    }
+                                                                },
+                                                                pass: {
+                                                                    color: [color],
+                                                                    depth_stencil: {}
+                                                                }
+    ).unwrap());
+
+    let pipeline = Arc::new(
+        GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vs.main_entry_point(), ())
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fs.main_entry_point(), ())
+            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
+
+    let dynamic_state = DynamicState {
+        viewports: Some(vec![Viewport {
+            origin: [0.0, 0.0],
+            dimensions: [1024.0, 1024.0],
+            depth_range: 0.0..1.0,
+        }]),
+        ..DynamicState::none()
+    };
+
+    let framebuffer = Arc::new(
+        Framebuffer::start(render_pass.clone())
+            .add(image.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let renderoutbuf = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage {
+            storage_buffer: true,
+            ..BufferUsage::transfer_destination()
+        },
+        false,
+        (0..1024 * 1024 * 4).map(|_| 0u8),
+    )
+    .expect("failed to create the image buffer");
+
+    let mut builder =
+        AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap();
+    builder
+        .begin_render_pass(
+            framebuffer.clone(),
+            false,
+            vec![[0.0, 0.0, 1.0, 1.0].into()],
+        )
+        .unwrap()
+        .draw(
+            pipeline.clone(),
+            &dynamic_state,
+            vertex_buffer.clone(),
+            (),
+            (),
+        )
+        .unwrap()
+        .end_render_pass()
+        .unwrap()
+        .copy_image_to_buffer(image.clone(), renderoutbuf.clone())
+        .unwrap();
+
+    let command_buffer = builder.build().unwrap();
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+
+    let rendercontents = renderoutbuf.read().unwrap();
+    let rendered = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &rendercontents[..]).unwrap();
+    rendered.save("gpu-triangle.png").unwrap();
 }
